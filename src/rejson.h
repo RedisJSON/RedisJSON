dec269266b6ab55e9452ecfdc778631bd672fce5
Itamar Haber <itamar@redislabs.com>
2017-05-16 14:14:29 +0300

    Hardcodes the project version in the rejson.h file for now
    This needs to be integrated into a proper version build system/
    script - TBD.

--- src/rejson.h
+++ src/rejson.h
@@ -22,7 +22,6 @@
 #include <sds.h>
 #include <string.h>
 #include <util.h>
-#include "config.h"
 #include "json_object.h"
 #include "json_path.h"
 #include "object.h"
@@ -31,6 +30,18 @@
 
 #define RLMODULE_NAME "ReJSON"
 #define RLMODULE_DESC "JSON data type for Redis"
+
+#ifndef PROJECT_VERSION_MAJOR
+#define PROJECT_VERSION_MAJOR 999
+#endif
+
+#ifndef PROJECT_VERSION_MINOR
+#define PROJECT_VERSION_MINOR 999
+#endif
+
+#ifndef PROJECT_VERSION_PATCH
+#define PROJECT_VERSION_PATCH 999
+#endif
 
 #define RM_ERRORMSG_SYNTAX "ERR syntax error"
 
