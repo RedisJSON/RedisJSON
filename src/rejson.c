5ebd76d24d14ac4a9b9005fb4fae27480fee4a5f
Guy Korland <gkorland@gmail.com>
2019-02-21 13:55:36 +0200

    Update rejson.c

--- src/rejson.c
+++ src/rejson.c
@@ -1,5 +1,5 @@
 /*
-* Copyright 2018-2019 Redis Labs Ltd. and Contributors
+* Copyright 2017-2019 Redis Labs Ltd. and Contributors
 *
 * This file is available under the Redis Labs Source Available License Agreement
 */
