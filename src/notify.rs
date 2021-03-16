7645684512d61651022a53633286f6a319e03839
oshadmi <omer.shadmi@redislabs.com>
2021-03-16 08:53:12 +0200

    WIP add getPath and initial getType API

--- src/notify.rs
+++ src/notify.rs
@@ -1,4 +1,4 @@
-use redis_module::{raw as rawmod, RedisString};
+use redis_module::{raw as rawmod, RedisError, RedisString};
 use redis_module::{Context, NotifyEvent, Status};
 use std::{
     ffi::CStr,
@@ -11,6 +11,7 @@ use crate::{
 };
 use std::ffi::CString;
 use std::os::raw::c_int;
+use std::ptr::{null, null_mut};
 
 //
 // structs
@@ -33,6 +34,19 @@ pub enum JSONType {
     Object, //(Map<String, Value>),
 }
 
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub enum JSONCType {
+    String = 0,
+    Int = 1,
+    Double = 2,
+    Bool = 3,
+    Object = 4,
+    Array = 5,
+    Null = 6,
+    Err = 7,
+}
+
 // #[repr(C)]
 // union JsonValueUnion {
 //     // err: int,
@@ -52,42 +66,47 @@ pub enum JSONType {
 // }
 
 #[no_mangle]
+pub extern "C" fn getInfo(
+    redisjson: *mut c_void,
+    name: *mut c_void,
+    jtype: *mut JSONCType,
+    size: *mut libc::size_t,
+) -> c_int {
+    if !redisjson.is_null() {
+        let json = unsafe { &*(redisjson as *mut RedisJSON) };
+        let t = json.get_type("");
+        match t {
+            Ok(o) => {
+                print!("{:?}\n", o)
+            }
+            Err(e) => {
+                print!("{:?}\n", e);
+            }
+        }
+    } else {
+        //
+    }
+    0
+}
+
+//FIXME: //TODO: Add free API for redisjson: *mut c_void
+
+#[no_mangle]
 pub extern "C" fn getPath(
     module_ctx: *mut rawmod::RedisModuleCtx,
     key_str: *mut rawmod::RedisModuleString,
     path: *const c_char,
-) -> *const rawmod::RedisModuleString {
+) -> *mut c_void {
     let ctx = Context::new(module_ctx);
     let key = ctx.open_with_redis_string(key_str);
-    let value = match key.get_value::<RedisJSON>(&REDIS_JSON_TYPE) {
-        Ok(v) => match v {
-            Some(doc) => {
-                print!("{:?}", doc);
-                let mut paths: Vec<Path> = Vec::new();
-                paths.push(Path::new(unsafe {
-                    CStr::from_ptr(path).to_str().unwrap().into()
-                }));
-                match doc.to_json(
-                    &mut paths,
-                    "".to_string(),
-                    "\n".to_string(),
-                    " ".to_string(),
-                    Format::JSON,
-                ) {
-                    Ok(doc) => doc,
-                    Err(e) => e.msg,
-                }
-            }
-            None => String::from("<key not found>"),
-        },
-        Err(_) => String::from("<err>"),
-    };
-    unsafe {
-        rawmod::RedisModule_CreateString.unwrap()(
-            ctx.get_ctx(),
-            value.as_ptr() as *const c_char,
-            value.len(),
-        )
+    if let Ok(res) = key.get_value::<RedisJSON>(&REDIS_JSON_TYPE) {
+        if let Some(value) = res {
+            Box::into_raw(Box::new(value)) as *mut c_void
+        } else {
+            null_mut()
+        }
+    } else {
+        null_mut()
     }
 }
 
@@ -95,14 +114,14 @@ static REDISJSON_GETAPI: &str = concat!("RedisJSON_V1", "\0");
 
 pub fn export_shared_api(ctx: &Context) {
     ctx.export_shared_api(
-        //RedisJSON_GetApiV1 as *const c_void,
-        RedisJSON_GetApiV1 as *const c_void,
+        &JSONAPI as *const RedisModuleAPI_V1 as *const c_void,
         REDISJSON_GETAPI.as_ptr() as *mut i8,
     );
 }
 
 static JSONAPI: RedisModuleAPI_V1 = RedisModuleAPI_V1 {
-    getPath: Some(getPath),
+    getPath: getPath,
+    getInfo: getInfo,
 };
 
 #[no_mangle]
@@ -122,13 +141,13 @@ pub extern "C" fn RedisJSON_GetApiV1(_module_ctx: *mut RedisModuleCtx) -> *const
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct RedisModuleAPI_V1 {
-    pub getPath: Option<
-        unsafe extern "C" fn(
-            module_ctx: *mut rawmod::RedisModuleCtx,
-            key_str: *mut rawmod::RedisModuleString,
-            path: *const c_char,
-        ) -> *const rawmod::RedisModuleString,
-    >,
+    pub getPath: unsafe extern "C" fn(
+        module_ctx: *mut rawmod::RedisModuleCtx,
+        key_str: *mut rawmod::RedisModuleString,
+        path: *const c_char,
+    ) -> *mut c_void,
+    pub getInfo:
+        unsafe extern "C" fn(*mut c_void, *mut c_void, *mut JSONCType, *mut libc::size_t) -> c_int,
 }
 
 pub fn notify_keyspace_event(
