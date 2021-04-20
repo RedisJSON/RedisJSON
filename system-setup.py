046bbc56006a411e2c4e4d5cd261b257151145ca
Guy Korland <gkorland@gmail.com>
2021-04-20 15:43:55 +0300

    install missing clang

--- system-setup.py
+++ system-setup.py
@@ -20,7 +20,7 @@ class RedisJSONSetup(paella.Setup):
         self.pip_install("wheel")
         self.pip_install("setuptools --upgrade")
 
-        self.install("git clang")
+        self.install("git llvm-dev libclang-dev clang")
         if not self.has_command("rustc"):
             self.run("%s/bin/getrust" % READIES)
         self.run("%s/bin/getcmake" % READIES)
