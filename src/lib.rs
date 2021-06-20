b6f3cbdd4b141a8e06ea9cf2985ce3a00ebdccd1
Guy Korland <gkorland@gmail.com>
2021-06-20 12:17:31 +0300

    format code

--- src/lib.rs
+++ src/lib.rs
@@ -232,9 +232,7 @@ fn json_get(ctx: &Context, args: Vec<String>) -> RedisResult {
 
     let key = ctx.open_key_writable(&key);
     let value = match key.get_value::<RedisJSON>(&REDIS_JSON_TYPE)? {
-        Some(doc) => doc
-            .to_json(paths, indent, newline, space, format)?
-            .into(),
+        Some(doc) => doc.to_json(paths, indent, newline, space, format)?.into(),
         None => RedisValue::Null,
     };
 
