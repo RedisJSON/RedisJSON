b8e8c280fc1bb7a1a67c425676acd763b4e27be4
oshadmi <omer.shadmi@redislabs.com>
2021-11-08 09:20:02 +0200

    Log version git branch and use short format of git sha

--- build.rs
+++ build.rs
@@ -2,9 +2,19 @@ use std::process::Command;
 
 fn main() {
     // Expose GIT_SHA env var
-    let gitsha = Command::new("git").args(&["rev-parse", "HEAD"]).output();
-    if let Ok(sha) = gitsha {
+    let git_sha = Command::new("git")
+        .args(&["rev-parse", "--short", "HEAD"])
+        .output();
+    if let Ok(sha) = git_sha {
         let sha = String::from_utf8(sha.stdout).unwrap();
         println!("cargo:rustc-env=GIT_SHA={}", sha);
+    }
+    // Expose GIT_BRANCH env var
+    let git_branch = Command::new("git")
+        .args(&["rev-parse", "--abbrev-ref", "HEAD"])
+        .output();
+    if let Ok(branch) = git_branch {
+        let branch = String::from_utf8(branch.stdout).unwrap();
+        println!("cargo:rustc-env=GIT_BRANCH={}", branch);
     }
 }
