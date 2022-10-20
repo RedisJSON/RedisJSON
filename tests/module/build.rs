8c3376beadb05bf5562d8478d0988e800236cdbd
Ephraim Feldblum <ephraim.feldblum@redis.com>
2022-10-20 16:02:41 +0300

    compiles. which is more than i could say yesterday.

---
+++ tests/module/build.rs
@@ -0,0 +1,32 @@
+extern crate bindgen;
+extern crate cc;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    const EXPERIMENTAL_API: &str = "REDISMODULE_EXPERIMENTAL_API";
+
+    cc::Build::new()
+        .define(EXPERIMENTAL_API, None)
+        .file("src/rejson_api.c")
+        .include("src/include/")
+        .compile("rejson_api");
+
+    let bindings = bindgen::Builder::default()
+        .clang_arg(format!("-D{}", EXPERIMENTAL_API).as_str())
+        .header("src/include/rejson_api.h")
+        // .allowlist_var("(REDIS|Redis).*")
+        .blocklist_type("__darwin_.*")
+        .blocklist_type("RedisModule.*")
+        .allowlist_type("(RedisJSON|JSON).*")
+        // .parse_callbacks(Box::new(RedisModuleCallback))
+        .size_t_is_usize(true)
+        .generate()
+        .expect("error generating bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("failed to write bindings to file");
+}
--- tests/module/src/lib.rs
+++ tests/module/src/lib.rs
@@ -1,9 +1,14 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+extern crate redis_module;
 
-use redis_module::{Context, RedisError, RedisResult, Status, RedisString, RedisValue, key::RedisKey, RedisModuleCtx, RedisModule_GetSharedAPI,
-    subscribe_to_server_event, RedisModuleEvent, RedisModuleEvent_ModuleChange, REDISMODULE_SUBEVENT_MODULE_LOADED, RedisModuleModuleChange};
-use std::ffi::{c_int, c_longlong, c_ulonglong, c_double, CStr, CString, c_void};
+use redis_module::*;
+use std::str;
 use std::f64::EPSILON;
+use std::ffi::{CStr, c_void};
 use cstr::cstr;
 use function_name::named;
 
@@ -11,230 +16,240 @@ const MODULE_NAME: &str = "RJ_LLAPI";
 const MODULE_VERSION: u32 = 1;
 
 struct RjApi {
-    japi: *const RedisJSONAPI,
-    version: i32,
+	japi: *const RedisJSONAPI,
+	version: i32,
 }
 
 impl RjApi {
-    pub fn api(&self) -> &RedisJSONAPI {
-        &*self.japi
-    }
+	pub unsafe fn api(&self) -> &RedisJSONAPI {
+		&*self.japi
+	}
 }
 
 static mut rj_api: RjApi = RjApi {
-    japi: std::ptr::null::<RedisJSONAPI>,
-    version: 0
+	japi: std::ptr::null::<RedisJSONAPI>(),
+	version: 0
 };
 
 fn get_json_apis(
-    ctx: *mut RedisModuleCtx,
-    subscribe_to_module_change: bool
+	ctx: *mut RedisModuleCtx,
+	subscribe_to_module_change: bool
 ) -> Status {
-    let japi: *mut ::std::os::raw::c_void;
+	let mut japi: *const std::os::raw::c_void;
 
-    japi = unsafe { RedisModule_GetSharedAPI.unwrap()(ctx, cstr!("RedisJSON_V2").as_ptr()) };
-    if !japi.is_null() {
-        rj_api.japi = japi as *const RedisJSONAPI;
-        rj_api.version = 2;
-        return Status::Ok;
-    }
-    
-    japi = unsafe { RedisModule_GetSharedAPI.unwrap()(ctx, cstr!("RedisJSON_V1").as_ptr()) };
-    if !japi.is_null() {
-        rj_api.japi = japi as *const RedisJSONAPI;
-        rj_api.version = 1;
-        return Status::Ok;
-    }
+	japi = unsafe { RedisModule_GetSharedAPI.unwrap()(ctx, cstr!("RedisJSON_V2").as_ptr()) };
+	if !japi.is_null() {
+		unsafe {
+			rj_api.japi = japi as *const RedisJSONAPI;
+			rj_api.version = 2;
+		}
+		return Status::Ok;
+	}
+	
+	japi = unsafe { RedisModule_GetSharedAPI.unwrap()(ctx, cstr!("RedisJSON_V1").as_ptr()) };
+	if !japi.is_null() {
+		unsafe {
+			rj_api.japi = japi as *const RedisJSONAPI;
+			rj_api.version = 1;
+		}
+		return Status::Ok;
+	}
 
-    if subscribe_to_module_change {
-        return subscribe_to_server_event(ctx, RedisModuleEvent_ModuleChange, Some(module_change_handler));
-    }
+	if subscribe_to_module_change {
+		return unsafe { subscribe_to_server_event(ctx, RedisModuleEvent_ModuleChange, Some(module_change_handler)) };
+	}
 
-    Status::Ok
+	Status::Ok
 }
 
 unsafe extern "C" fn module_change_handler(
-    ctx: *mut RedisModuleCtx,
-    event: RedisModuleEvent,
-    sub: u64,
-    ei: *mut c_void
+	ctx: *mut RedisModuleCtx,
+	_event: RedisModuleEvent,
+	sub: u64,
+	ei: *mut c_void
 ) {
-    let ei = &*(ei as *mut RedisModuleModuleChange);
-    if sub == REDISMODULE_SUBEVENT_MODULE_LOADED && // If the subscribed event is a module load,
-       rj_api.japi.is_null() &&                     // and JSON is not already loaded,
-       CStr::from_ptr(ei.module_name)
-            .to_str().unwrap() == "ReJSON" &&       // and the loading module is JSON:
-       get_json_apis(ctx, false) == Status::Err     // try to load it.
-    {
-        // Log Error
-    }
+	let ei = &*(ei as *mut RedisModuleModuleChange);
+	if sub == REDISMODULE_SUBEVENT_MODULE_LOADED && // If the subscribed event is a module load,
+		rj_api.japi.is_null() &&                      // and JSON is not already loaded,
+		CStr::from_ptr(ei.module_name)
+			.to_str().unwrap() == "ReJSON" &&           // and the loading module is JSON:
+		get_json_apis(ctx, false) == Status::Err      // try to load it.
+	{
+			// Log Error
+	}
 }
 
 fn init(ctx: &Context, _args: &[RedisString]) -> Status {
-    get_json_apis(ctx.ctx, true);
-    Status::Ok
+	get_json_apis(ctx.ctx, true);
+	Status::Ok
 }
 
 #[named]
 fn RJ_llapi_test_open_key(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    if args.len() != 1 {
-        return Err(RedisError::WrongArity);
-    }
+	if args.len() != 1 {
+		return Err(RedisError::WrongArity);
+	}
 
-    let keyname = RedisString::create(ctx.ctx, function_name!());
+	let keyname = RedisString::create(ctx.ctx, function_name!());
 
-    assert!(ctx.call("JSON.SET", &[function_name!(), "$", "0"]).is_ok());
-    let rmk = RedisKey::open(ctx.ctx, &keyname);
-    assert_eq!(unsafe { rj_api.api().isJSON.unwrap()(rmk.key_inner) }, 1);
-    assert!(unsafe { !(rj_api.api().openKey.unwrap()(ctx, &keyname).is_null()) });
+	assert!(ctx.call("JSON.SET", &[function_name!(), "$", "0"]).is_ok());
+	let rmk = key::RedisKey::open(ctx.ctx, &keyname);
+	assert_eq!(unsafe { rj_api.api().isJSON.unwrap()(rmk.key_inner) }, 1);
+	assert!(unsafe { !(rj_api.api().openKey.unwrap()(ctx.ctx, keyname.inner).is_null()) });
 
-    ctx.call("SET", &[function_name!(), "0"]);
-    rmk = RedisKey::open(ctx.ctx, &keyname);
-    assert_ne!(unsafe { rj_api.api().isJSON.unwrap()(rmk.key_inner) }, 1);
-    assert!(unsafe { rj_api.api().openKey.unwrap()(ctx, &keyname).is_null() });
+	ctx.call("SET", &[function_name!(), "0"]).unwrap();
+	let rmk = key::RedisKey::open(ctx.ctx, &keyname);
+	assert_ne!(unsafe { rj_api.api().isJSON.unwrap()(rmk.key_inner) }, 1);
+	assert!(unsafe { rj_api.api().openKey.unwrap()(ctx.ctx, keyname.inner).is_null() });
 
-    Ok(RedisValue::SimpleStringStatic("PASS"))
+	Ok(RedisValue::SimpleStringStatic("PASS"))
 }
 
 #[named]
 fn RJ_llapi_test_iterator(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    if args.len() != 1 {
-        return Err(RedisError::WrongArity);
-    }
+	if args.len() != 1 {
+		return Err(RedisError::WrongArity);
+	}
 
-    let _keyname = RedisString::create(ctx.ctx, function_name!());
+	let keyname = RedisString::create(ctx.ctx, function_name!());
 
-    // #define VALS 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    // long long vals[] = { VALS };
-    // const char json[] = "[" STRINGIFY(VALS) "]";
-    // RedisModule_Call(ctx, "JSON.SET", "ccc", TEST_NAME, "$", json);
-  
-    // JSONResultsIterator ji = RjApi.japi->get(RjApi.japi->openKeyFromStr(ctx, TEST_NAME), "$..*");
-    // ASSERT(ji != NULL);
-    // if (RjApi.version >= 2) {
-    //   RedisModuleString *str;
-    //   RjApi.japi->getJSONFromIter(ji, ctx, &str);
-    //   ASSERT(strcmp(RedisModule_StringPtrLen(str, NULL), json) == 0);
-    //   RedisModule_FreeString(ctx, str);
-    // }
-  
-    // size_t len = RjApi.japi->len(ji); ASSERT(len == sizeof(vals)/sizeof(*vals));
-    // RedisJSON js; long long num;
-    // for (int i = 0; i < len; ++i) {
-    //   js = RjApi.japi->next(ji); ASSERT(js != NULL);
-    //   RjApi.japi->getInt(js, &num); ASSERT(num == vals[i]);
-    // }
-    // ASSERT(RjApi.japi->next(ji) == NULL);
-  
-    // RjApi.japi->freeIter(ji);
+	let vals =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] ;
+	let json = "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]";
+	ctx.call("JSON.SET", &[function_name!(), "$", json]).unwrap();
 
-    Ok(RedisValue::SimpleStringStatic("PASS"))
+	let ji = unsafe { rj_api.api().get.unwrap()(rj_api.api().openKey.unwrap()(ctx.ctx, keyname.inner), cstr!("$..*").as_ptr()) };
+	assert!(!ji.is_null());
+	unsafe { if rj_api.version >= 2 {
+		let mut s = RedisString::create(ctx.ctx, "");
+		rj_api.api().getJSONFromIter.unwrap()(ji, ctx.ctx, &mut s.inner as *mut _);
+		// let s = RedisModule_StringPtrLen.unwrap()(s.inner, 0 as *mut _).to_str().unwrap();
+		// assert_eq!(s, json);
+	}}
+	// JSONResultsIterator ji = RjApi.japi->get(RjApi.japi->openKeyFromStr(ctx, TEST_NAME), "$..*");
+	// ASSERT(ji != NULL);
+	// if (RjApi.version >= 2) {
+	//   RedisModuleString *str;
+	//   RjApi.japi->getJSONFromIter(ji, ctx, &str);
+	//   ASSERT(strcmp(RedisModule_StringPtrLen(str, NULL), json) == 0);
+	//   RedisModule_FreeString(ctx, str);
+	// }
+
+	// size_t len = RjApi.japi->len(ji); ASSERT(len == sizeof(vals)/sizeof(*vals));
+	// RedisJSON js; long long num;
+	// for (int i = 0; i < len; ++i) {
+	//   js = RjApi.japi->next(ji); ASSERT(js != NULL);
+	//   RjApi.japi->getInt(js, &num); ASSERT(num == vals[i]);
+	// }
+	// ASSERT(RjApi.japi->next(ji) == NULL);
+
+	// RjApi.japi->freeIter(ji);
+
+	Ok(RedisValue::SimpleStringStatic("PASS"))
 }
 
 #[named]
 fn RJ_llapi_test_get_type(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    if args.len() != 1 {
-        return Err(RedisError::WrongArity);
-    }
+	if args.len() != 1 {
+		return Err(RedisError::WrongArity);
+	}
 
-    let keyname = RedisString::create(ctx.ctx, function_name!());
+	let keyname = RedisString::create(ctx.ctx, function_name!());
 
-    ctx.call("JSON.SET", &[function_name!(), "$", "[\"\", 0, 0.0, false, {}, [], null]"]);
-    let js = unsafe { rj_api.api().openKey.unwrap()(ctx, &keyname) };
-    
-    let mut len = 0u64;
-    unsafe { rj_api.api().getLen(js, &mut len as *mut c_ulonglong) };
-    assert_eq!(len, JSONType__EOF as u64);
+	ctx.call("JSON.SET", &[function_name!(), "$", "[\"\", 0, 0.0, false, {}, [], null]"]).unwrap();
+	let js = unsafe { rj_api.api().openKey.unwrap()(ctx.ctx, keyname.inner) };
+	
+	let mut len = 0usize;
+	unsafe { rj_api.api().getLen.unwrap()(js, &mut len as *mut _) };
+	assert_eq!(len, JSONType_JSONType__EOF as usize);
 
-    for i in 0..len { unsafe { 
-        let elem = rj_api.api().getAt.unwrap()(js, i as c_ulonglong);
-        let jtype = rj_api.api().getType.unwrap()(elem);
-        assert_eq!(jtype, i as c_int);
-    }}
+	for i in 0..len { unsafe { 
+		let elem = rj_api.api().getAt.unwrap()(js, i);
+		let jtype = rj_api.api().getType.unwrap()(elem);
+		assert_eq!(jtype, i as u32);
+	}}
 
-    Ok(RedisValue::SimpleStringStatic("PASS"))
+	Ok(RedisValue::SimpleStringStatic("PASS"))
 }
 
 #[named]
 fn RJ_llapi_test_get_value(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    if args.len() != 1 {
-        return Err(RedisError::WrongArity);
-    }
+	if args.len() != 1 {
+		return Err(RedisError::WrongArity);
+	}
 
-    let keyname = RedisString::create(ctx.ctx, function_name!());
+	let keyname = RedisString::create(ctx.ctx, function_name!());
 
-    ctx.call("JSON.SET", &[function_name!(), "$", "[\"a\", 1, 0.1, true, {\"_\":1}, [1], null]"]);
-    let js = unsafe { rj_api.api().openKey.unwrap()(ctx, &keyname) };
-  
-    let mut s: CString;
-    let mut len: u64;
-    unsafe { rj_api.api().getString.unwrap()(rj_api.api().getAt.unwrap()(js, 0 as c_int), &mut s as *mut CString, &mut len as *mut c_ulonglong) };
-    assert_eq!(s.to_str().unwrap(), "a");
-  
-    let mut ll: i64;
-    unsafe { rj_api.api().getInt.unwrap()(rj_api.api().getAt.unwrap()(js, 1 as c_int), &mut ll as *mut c_longlong) };
-    assert_eq!(ll, 1);
-  
-    let mut dbl: f64;
-    unsafe { rj_api.api().getDouble.unwrap()(rj_api.api().getAt.unwrap()(js, 2 as c_int), &mut dbl as *mut c_double) };
-    assert!((dbl - 0.1).abs() < EPSILON);
+	ctx.call("JSON.SET", &[function_name!(), "$", "[\"a\", 1, 0.1, true, {\"_\":1}, [1], null]"]).unwrap();
+	let js = unsafe { rj_api.api().openKey.unwrap()(ctx.ctx, keyname.inner) };
 
-    let mut b: i32;
-    unsafe { rj_api.api().getBoolean.unwrap()(rj_api.api().getAt.unwrap()(js, 3 as c_int), &mut b as *mut c_int) };
-    assert_eq!(b, 1);
-  
-    len = 0;
-    unsafe { rj_api.api().getLen.unwrap()(rj_api.api().getAt.unwrap()(js, 4 as c_int), &mut len as *mut c_ulonglong) };
-    assert_eq!(len, 1);
-  
-    len = 0;
-    unsafe { rj_api.api().getLen.unwrap()(rj_api.api().getAt.unwrap()(js, 5 as c_int), &mut len as *mut c_ulonglong) };
-    assert_eq!(len, 1);
-  
-    Ok(RedisValue::SimpleStringStatic("PASS"))
+	let s = cstr!("");
+	let mut len = 0usize;
+	unsafe { rj_api.api().getString.unwrap()(rj_api.api().getAt.unwrap()(js, 0), &mut (s.as_ptr()) as *mut _, &mut len as *mut _) };
+	assert_eq!(s.to_str().unwrap(), "a");
+
+	let mut ll = 0i64;
+	unsafe { rj_api.api().getInt.unwrap()(rj_api.api().getAt.unwrap()(js, 1), &mut ll as *mut _) };
+	assert_eq!(ll, 1);
+
+	let mut dbl = 0f64;
+	unsafe { rj_api.api().getDouble.unwrap()(rj_api.api().getAt.unwrap()(js, 2), &mut dbl as *mut _) };
+	assert!((dbl - 0.1).abs() < EPSILON);
+
+	let mut b = 0i32;
+	unsafe { rj_api.api().getBoolean.unwrap()(rj_api.api().getAt.unwrap()(js, 3), &mut b as *mut _) };
+	assert_eq!(b, 1);
+
+	len = 0;
+	unsafe { rj_api.api().getLen.unwrap()(rj_api.api().getAt.unwrap()(js, 4), &mut len as *mut _) };
+	assert_eq!(len, 1);
+
+	len = 0;
+	unsafe { rj_api.api().getLen.unwrap()(rj_api.api().getAt.unwrap()(js, 5), &mut len as *mut _) };
+	assert_eq!(len, 1);
+
+	Ok(RedisValue::SimpleStringStatic("PASS"))
 }
 
 
 const fn split(cmd: &str) -> (&str, &str) {
-    use ::konst::option::unwrap;
-    use ::konst::slice::get_range;
-    use ::konst::slice::get_from;
+	use konst::option::unwrap;
+	use konst::slice::get_range;
+	use konst::slice::get_from;
 
-    const i: usize = MODULE_NAME.len();
-    let cmd = cmd.as_bytes();
-    let (hd, tl) = (
-        unwrap!(get_range(cmd, 0, i)),
-        unwrap!(get_from(cmd, i + "_".len())),
-    );
-    unsafe { (
-        ::core::str::from_utf8_unchecked(hd),
-        ::core::str::from_utf8_unchecked(tl),
-    )}
+	const i: usize = MODULE_NAME.len();
+	let cmd = cmd.as_bytes();
+	let (hd, tl) = (
+		unwrap!(get_range(cmd, 0, i)),
+		unwrap!(get_from(cmd, i + "_".len())),
+	);
+	unsafe {(
+		core::str::from_utf8_unchecked(hd),
+		core::str::from_utf8_unchecked(tl),
+	)}
 }
 
 macro_rules! my_module {
-    ($( $cmd:expr, )*) => {
-        redis_module! (
-            name: MODULE_NAME,
-            version: MODULE_VERSION,
-            data_types: [],
-            init: init,
-            commands: [
-                $({
-                    const NAME: &str = {
-                        const SPLIT: (&str, &str) = split(stringify!($cmd));
-                        ::const_format::concatcp!(SPLIT.0, ".", SPLIT.1);
-                    }
-                    [NAME, $cmd, "", 0, 0, 0],
-                })*
-            ]
-        );
-    }
+	($( $cmd:expr, )*) => {
+		redis_module! (
+			name: MODULE_NAME,
+			version: MODULE_VERSION,
+			data_types: [],
+			init: init,
+			commands: [
+				$(
+					[{
+						const SPLIT: (&str, &str) = split(stringify!($cmd));
+						const_format::concatcp!(SPLIT.0, ".", SPLIT.1)
+					}, $cmd, "", 0, 0, 0],
+				)*
+			]
+		);
+	}
 }
 
 my_module! {
-    RJ_llapi_test_open_key,
-    RJ_llapi_test_iterator,
-    RJ_llapi_test_get_type,
-    RJ_llapi_test_get_value,
+	RJ_llapi_test_open_key,
+	RJ_llapi_test_iterator,
+	RJ_llapi_test_get_type,
+	RJ_llapi_test_get_value,
 }
