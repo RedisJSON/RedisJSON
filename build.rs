fe9bfc2d90dda9700154b1ec9aca93db1c2b0d2e
Guy Korland <gkorland@gmail.com>
2022-11-09 18:12:36 +0200

    clean more clippy warnings

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     // Expose GIT_SHA env var
     let git_sha = Command::new("git")
-        .args(&["rev-parse", "--short", "HEAD"])
+        .args(["rev-parse", "--short", "HEAD"])
         .output();
     if let Ok(sha) = git_sha {
         let sha = String::from_utf8(sha.stdout).unwrap();
@@ -11,7 +11,7 @@ fn main() {
     }
     // Expose GIT_BRANCH env var
     let git_branch = Command::new("git")
-        .args(&["rev-parse", "--abbrev-ref", "HEAD"])
+        .args(["rev-parse", "--abbrev-ref", "HEAD"])
         .output();
     if let Ok(branch) = git_branch {
         let branch = String::from_utf8(branch.stdout).unwrap();
