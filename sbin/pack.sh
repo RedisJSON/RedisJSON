2459620e8f741bb8229f52c020e7f1d18deecfa8
Rafi Einstein <rafi@redislabs.com>
2023-02-23 17:02:22 +0200

    Amazon Linux 2 support (#928)
    (cherry picked from commit 7dc22197a455a47584aa29f13089fd53d02b9db4)

--- sbin/pack.sh
+++ sbin/pack.sh
@@ -21,27 +21,35 @@ if [[ $1 == --help || $1 == help || $HELP == 1 ]]; then
 		[ARGVARS...] pack.sh [--help|help]
 
 		Argument variables:
-		MODULE=path       Path of module .so
+		MODULE=path         Path of module .so
 
-		RAMP=1|0          Build RAMP file
-		DEPS=0|1          Build dependencies file
-		SYM=0|1           Build debug symbols file
+		RAMP=0|1            Build RAMP package
+		DEPS=0|1            Build dependencies files
+		SYM=0|1             Build debug symbols file
 
-		BRANCH=name       Branch name for snapshot packages
-		WITH_GITSHA=1     Append Git SHA to shapshot package names
-		VARIANT=name      Build variant (default: empty)
+		BRANCH=name         Branch name for snapshot packages
+		WITH_GITSHA=1       Append Git SHA to shapshot package names
+		VARIANT=name        Build variant
+		RAMP_VARIANT=name   RAMP variant (e.g. ramp-{name}.yml)
 
-		ARTDIR=dir        Directory in which packages are created (default: bin/artifacts)
+		ARTDIR=dir          Directory in which packages are created (default: bin/artifacts)
+		
+		RAMP_YAML=path      RAMP configuration file path
+		RAMP_ARGS=args      Extra arguments to RAMP
 
-		JUST_PRINT=1      Only print package names, do not generate
-		VERBOSE=1         Print commands
-		IGNERR=1          Do not abort on error
+		JUST_PRINT=1        Only print package names, do not generate
+		VERBOSE=1           Print commands
+		HELP=1              Show help
+		NOP=1               Print commands, do not execute
 
 	END
 	exit 0
 fi
 
 #----------------------------------------------------------------------------------------------
+
+OP=""
+[[ $NOP == 1 ]] && OP=echo
 
 # RLEC naming conventions
 
@@ -64,25 +72,27 @@ OSNICK=$($READIES/bin/platform --osnick)
 
 [[ $OSNICK == bigsur ]]  && OSNICK=catalina
 
+PLATFORM="$OS-$OSNICK-$ARCH"
+
 #----------------------------------------------------------------------------------------------
 
 if [[ -z $MODULE || ! -f $MODULE ]]; then
-	eprint "MODULE is undefined or is not referring to a file"
+	eprint "MODULE is not defined or does not refer to a file"
 	exit 1
 fi
 
 RAMP=${RAMP:-1}
 DEPS=${DEPS:-1}
 SYM=${SYM:-1}
-DEPNAMES=""
 
 [[ -z $ARTDIR ]] && ARTDIR=bin/artifacts
 mkdir -p $ARTDIR $ARTDIR/snapshots
 ARTDIR=$(cd $ARTDIR && pwd)
 
-export DEPNAMES=""
-
+MODULE_NAME=${MODULE_NAME:-ReJSON}
 PACKAGE_NAME=rejson-oss
+
+DEP_NAMES=""
 
 RAMP_CMD="python3 -m RAMP.ramp"
 
@@ -91,10 +101,9 @@ RAMP_CMD="python3 -m RAMP.ramp"
 pack_ramp() {
 	cd $ROOT
 
-	local platform="$OS-$OSNICK-$ARCH"
-	local stem=${PACKAGE_NAME}.${platform}
+	local stem=${PACKAGE_NAME}.${PLATFORM}
 
-	local verspec=${SEMVER}${VARIANT}
+	local verspec=${SEMVER}${_VARIANT}
 	
 	local fq_package=$stem.${verspec}.zip
 
@@ -103,26 +112,55 @@ pack_ramp() {
 	local packfile="$ARTDIR/$fq_package"
 
 	local xtx_vars=""
-	local dep_fname=${PACKAGE_NAME}.${platform}.${verspec}.tgz
+	local dep_fname="${PACKAGE_NAME}.${PLATFORM}.${verspec}.tgz"
 
-	local rampfile=ramp.yml
+	if [[ -z $RAMP_YAML ]]; then
+		RAMP_YAML=$ROOT/ramp.yml
+	elif [[ -z $RAMP_VARIANT ]]; then
+		RAMP_YAML=$ROOT/ramp.yml
+	else
+		RAMP_YAML=$ROOT/ramp${_RAMP_VARIANT}.yml
+	fi
 
 	python3 $READIES/bin/xtx \
 		$xtx_vars \
 		-e NUMVER -e SEMVER \
-		$ROOT/$rampfile > /tmp/ramp.yml
-	rm -f /tmp/ramp.fname $packfile
-	$RAMP_CMD pack -m /tmp/ramp.yml --packname-file /tmp/ramp.fname --verbose --debug \
-		-o $packfile $MODULE >/tmp/ramp.err 2>&1 || true
-	if [[ ! -e $packfile ]]; then
-		eprint "Error generating RAMP file:"
-		>&2 cat /tmp/ramp.err
-		exit 1
+		$RAMP_YAML > /tmp/ramp.yml
+	if [[ $VERBOSE == 1 ]]; then
+		echo "# ramp.yml:"
+		cat /tmp/ramp.yml
+	fi
+
+	runn rm -f /tmp/ramp.fname $packfile
+	
+	# ROOT is required so ramp will detect the right git commit
+	cd $ROOT
+	runn @ <<-EOF
+		$RAMP_CMD pack -m /tmp/ramp.yml \
+			$RAMP_ARGS \
+			-n $MODULE_NAME \
+			--verbose \
+			--debug \
+			--packname-file /tmp/ramp.fname \
+			-o $packfile \
+			$MODULE \
+			>/tmp/ramp.err 2>&1 || true
+		EOF
+
+	if [[ $NOP != 1 ]]; then
+		if [[ ! -e $packfile ]]; then
+			eprint "Error generating RAMP file:"
+			>&2 cat /tmp/ramp.err
+			exit 1
+		else
+			local packname=`cat /tmp/ramp.fname`
+			echo "# Created $packname"
+		fi
 	fi
 
 	cd $ARTDIR/snapshots
 	if [[ ! -z $BRANCH ]]; then
-		local snap_package=$stem.${BRANCH}${VARIANT}.zip
+		local snap_package=$stem.${BRANCH}${_VARIANT}.zip
 		ln -sf ../$fq_package $snap_package
 	fi
 
@@ -136,9 +174,8 @@ pack_ramp() {
 pack_deps() {
 	local dep="$1"
 
-	local platform="$OS-$OSNICK-$ARCH"
-	local stem=${PACKAGE_NAME}.${dep}.${platform}
-	local verspec=${SEMVER}${VARIANT}
+	local stem=${PACKAGE_NAME}.${dep}.${PLATFORM}
+	local verspec=${SEMVER}${_VARIANT}
 
 	local depdir=$(cat $ARTDIR/$dep.dir)
 
@@ -159,15 +196,17 @@ pack_deps() {
 		cat /tmp/pack.err >&2
 		exit 1
 	fi
-	sha256sum $tar_path | awk '{print $1}' > $tar_path.sha256
+	runn @ <<-EOF
+		sha256sum $tar_path | awk '{print $1}' > $tar_path.sha256
+		EOF
 
 	cd $ARTDIR/snapshots
 	if [[ -n $BRANCH ]]; then
-		local snap_dep=$stem.${BRANCH}${VARIANT}.tgz
-		ln -sf ../$fq_dep $snap_dep
-		ln -sf ../$fq_dep.sha256 $snap_dep.sha256
+		local snap_dep=$stem.${BRANCH}${_VARIANT}.tgz
+		runn ln -sf ../$fq_dep $snap_dep
+		runn ln -sf ../$fq_dep.sha256 $snap_dep.sha256
 	fi
-	
+
 	cd $ROOT
 }
 
@@ -175,21 +214,24 @@ pack_deps() {
 
 prepare_symbols_dep() {
 	if [[ ! -f $MODULE.debug ]]; then return 0; fi
-	echo "Preparing debug symbols dependencies ..."
+	echo "# Preparing debug symbols dependencies ..."
 	echo $(cd "$(dirname $MODULE)" && pwd) > $ARTDIR/debug.dir
 	echo $(basename $MODULE.debug) > $ARTDIR/debug.files
 	echo "" > $ARTDIR/debug.prefix
 	pack_deps debug
-	echo "Done."
+	echo "# Done."
 }
 
 #----------------------------------------------------------------------------------------------
 
-NUMVER=$(NUMERIC=1 $SBIN/getver)
-SEMVER=$($SBIN/getver)
+NUMVER="$(NUMERIC=1 $SBIN/getver)"
+SEMVER="$($SBIN/getver)"
 
-if [[ ! -z $VARIANT ]]; then
-	VARIANT=-${VARIANT}
+if [[ -n $VARIANT ]]; then
+	_VARIANT="-${VARIANT}"
+fi
+if [[ ! -z $RAMP_VARIANT ]]; then
+	_RAMP_VARIANT="-${RAMP_VARIANT}"
 fi
 
 #----------------------------------------------------------------------------------------------
@@ -215,7 +257,7 @@ if [[ $JUST_PRINT == 1 ]]; then
 		echo "${PACKAGE_NAME}.${OS}-${OSNICK}-${ARCH}.${SEMVER}${VARIANT}.zip"
 	fi
 	if [[ $DEPS == 1 ]]; then
-		for dep in $DEPNAMES; do
+		for dep in $DEP_NAMES; do
 			echo "${PACKAGE_NAME}.${dep}.${OS}-${OSNICK}-${ARCH}.${SEMVER}${VARIANT}.tgz"
 		done
 	fi
@@ -224,31 +266,38 @@ fi
 
 #----------------------------------------------------------------------------------------------
 
+mkdir -p $ARTDIR
+
 if [[ $DEPS == 1 ]]; then
-	echo "Building dependencies ..."
+	echo "# Building dependencies ..."
 
 	[[ $SYM == 1 ]] && prepare_symbols_dep
 
-	for dep in $DEPNAMES; do
-			echo "$dep ..."
-			pack_deps $dep
+	for dep in $DEP_NAMES; do
+		echo "# $dep ..."
+		pack_deps $dep
 	done
+	echo "# Done."
 fi
+
+#----------------------------------------------------------------------------------------------
+
+cd $ROOT
 
 if [[ $RAMP == 1 ]]; then
 	if ! command -v redis-server > /dev/null; then
-		eprint "$PROGNAME: Cannot find redis-server. Aborting."
+		eprint "Cannot find redis-server. Aborting."
 		exit 1
 	fi
 
-	echo "Building RAMP $VARIANT files ..."
+	echo "# Building RAMP $RAMP_VARIANT files ..."
 	pack_ramp
-	echo "Done."
+	echo "# Done."
 fi
 
 if [[ $VERBOSE == 1 ]]; then
-	echo "Artifacts:"
-	du -ah --apparent-size $ARTDIR
+	echo "# Artifacts:"
+	$OP du -ah --apparent-size $ARTDIR
 fi
 
 exit 0
