3ded1b22951e25fe98d9f013a387be6ce24a408b
Guy Korland <gkorland@gmail.com>
2023-02-28 16:55:54 +0200

    use expected instead of panic

--- src/jsonpath/json_path.rs
+++ src/jsonpath/json_path.rs
@@ -45,22 +45,23 @@ impl<'i> Query<'i> {
     /// ($.foo)
     #[allow(dead_code)]
     pub fn pop_last(&mut self) -> Option<(String, JsonPathToken)> {
-        let last = self.root.next_back();
-        match last {
-            Some(last) => match last.as_rule() {
-                Rule::literal => Some((last.as_str().to_string(), JsonPathToken::String)),
-                Rule::number => Some((last.as_str().to_string(), JsonPathToken::Number)),
-                Rule::numbers_list => {
-                    let first_on_list = last.into_inner().next();
-                    first_on_list.map(|first| (first.as_str().to_string(), JsonPathToken::Number))
-                }
-                Rule::string_list => {
-                    let first_on_list = last.into_inner().next();
-                    first_on_list.map(|first| (first.as_str().to_string(), JsonPathToken::String))
-                }
-                _ => panic!("pop last was used in a none static path"),
-            },
-            None => None,
+        let last = self
+            .root
+            .next_back()
+            .expect("pop last was used in a none static path");
+
+        match last.as_rule() {
+            Rule::literal => Some((last.as_str().to_string(), JsonPathToken::String)),
+            Rule::number => Some((last.as_str().to_string(), JsonPathToken::Number)),
+            Rule::numbers_list => {
+                let first_on_list = last.into_inner().next();
+                first_on_list.map(|first| (first.as_str().to_string(), JsonPathToken::Number))
+            }
+            Rule::string_list => {
+                let first_on_list = last.into_inner().next();
+                first_on_list.map(|first| (first.as_str().to_string(), JsonPathToken::String))
+            }
+            _ => panic!(),
         }
     }
 
