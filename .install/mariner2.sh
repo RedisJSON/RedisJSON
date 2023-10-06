48517c2196f29072bcb233e6920105cb043b9425
nafraf <nafraf@gmail.com>
2023-10-06 14:17:12 -0500

    debug upload file

--- .install/mariner2.sh
+++ .install/mariner2.sh
@@ -3,12 +3,15 @@
 tdnf install -q -y build-essential git wget ca-certificates tar openssl-devel \
     cmake python3 python3-pip rust clang which unzip
 
-pip install --upgrade setuptools
-pip install --upgrade pip
+pip install -q --upgrade setuptools
+pip install -q --upgrade pip
 pip install -q -r tests/pytest/requirements.txt
 
 # These packages are needed to build the package
 pip install -q addict toml jinja2 ramp-packer
+
+# List installed packages
+pip list
 
 # Install aws-cli for uploading artifacts to s3
 curdir="$PWD"
