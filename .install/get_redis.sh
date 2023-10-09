81919656e35e660e3aa3a8d4c1520e7f31a1fc0d
nafraf <nafraf@gmail.com>
2023-10-09 09:54:30 -0500

    Use RLTest ~= 0.7.2

--- .install/get_redis.sh
+++ .install/get_redis.sh
@@ -8,10 +8,10 @@ cd /tmp/
 wget https://github.com/redis/redis/archive/${REDIS_VERSION}.tar.gz
 tar -xvzf ${REDIS_VERSION}.tar.gz && cd ./redis-${REDIS_VERSION}
 
-#patch
-sed -ri 's/(createEnum.*enable-protected-configs.*PROTECTED_ACTION_ALLOWED)_NO/\1_YES/g' ./src/config.c
-sed -ri 's/(createEnum.*enable-debug-command.*PROTECTED_ACTION_ALLOWED)_NO/\1_YES/g' ./src/config.c
-sed -ri 's/(createEnum.*enable-module-command.*PROTECTED_ACTION_ALLOWED)_NO/\1_YES/g' ./src/config.c
+# #patch
+# sed -ri 's/(createEnum.*enable-protected-configs.*PROTECTED_ACTION_ALLOWED)_NO/\1_YES/g' ./src/config.c
+# sed -ri 's/(createEnum.*enable-debug-command.*PROTECTED_ACTION_ALLOWED)_NO/\1_YES/g' ./src/config.c
+# sed -ri 's/(createEnum.*enable-module-command.*PROTECTED_ACTION_ALLOWED)_NO/\1_YES/g' ./src/config.c
 
 # Build redis from source and install it.
 make && make install
