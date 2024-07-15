1553712430adc51667ac8c675151e023a92b8490
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-07-15 16:52:00 +0300

    fix

--- .install/macos-12.sh
+++ .install/macos-12.sh
@@ -2,6 +2,24 @@
 
 echo "::error $(make --version)"
 
+export HOMEBREW_NO_AUTO_UPDATE=1
+BREW_PREFIX=$(brew --prefix)
+GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
+LLVM=$BREW_PREFIX/opt/llvm@16/bin
+COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
+
+brew update
+brew install coreutils
+brew install make
+brew install llvm@16
+
+echo "export PATH=$COREUTILS:$LLVM:$GNUBIN:$PATH" >> ~/.bashrc
+echo "export PATH=$COREUTILS:$LLVM:$GNUBIN:$PATH" >> ~/.zshrc
+source ~/.bashrc
+source ~/.zshrc
+
+brew install openssl
+
 version=3.25.1
 processor=$(uname -m)
 OS_TYPE=$(uname -s)
