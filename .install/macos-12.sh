b38e2ef3372f7671297493d5fe64028931608ed7
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-07-15 17:13:24 +0300

    add RLTest

--- .install/macos-12.sh
+++ .install/macos-12.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "::error $(make --version)"
-
 export HOMEBREW_NO_AUTO_UPDATE=1
 BREW_PREFIX=$(brew --prefix)
 GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
@@ -19,27 +17,4 @@ source ~/.bashrc
 source ~/.zshrc
 
 brew install openssl
-
-version=3.25.1
-processor=$(uname -m)
-OS_TYPE=$(uname -s)
-MODE=$1 # whether to install using sudo or not
-
-if [[ $OS_TYPE = 'Darwin' ]]
-then
-    brew install cmake
-else
-    if [[ $processor = 'x86_64' ]]
-    then
-        filename=cmake-${version}-linux-x86_64.sh
-    else
-        filename=cmake-${version}-linux-aarch64.sh
-    fi
-
-    wget https://github.com/Kitware/CMake/releases/download/v${version}/${filename}
-    chmod u+x ./${filename}
-    $MODE ./${filename} --skip-license --prefix=/usr/local --exclude-subdir
-    cmake --version
-fi
-
-echo "::error $(make --version)"
+source install_cmake.sh
