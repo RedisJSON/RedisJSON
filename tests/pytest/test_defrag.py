84606d25fb6d7b79732dbec01cc70c6181bbce2d
Meir Shpilraien <meir@redis.com>
2024-09-03 13:43:28 +0300

    RED-134847 support for active defrag for RedisJSON.
    The PR adds support for active defrag on RedisJSON. A pre condition for this PR is that the following PR's will be megred:
    
    * [Redis defrad module API extentions](https://github.com/redis/redis/pull/13509)
    * [redismodule-rs support for active defrag API](https://github.com/RedisLabsModules/redismodule-rs/pull/387)
    * [IJSON support for defrag](https://github.com/RedisJSON/ijson/pull/1)
    
    The PR register defrag function on the json datatype and uses the new capability of ISON to defrag the key.
    
    **Notice**:
    
    * that increamental defrag of the json key is **not** support. In order to support it we need to implement the free_effort callback. This is not trivial and even if it was it would have cause the json object to potentially be freed when the GIL is not hold, which violate the assumption of our shared string implementation (it is not thread safe). We leave it for future improvment.
    
    * If we run on a Redis version that do not support the defrag start callback, we can still partially support defrag. In that case the IJSON object will be defraged but the shared strings dictionaty will not be reinitialize. This basically means that shared strings will not be defraged.
    
    Tests were added to cover the new functionality.

---
+++ tests/pytest/test_defrag.py
@@ -0,0 +1,83 @@
+import time
+import json
+from RLTest import Defaults
+
+Defaults.decode_responses = True
+
+def enableDefrag(env):
+    # make defrag as aggressive as possible
+    env.cmd('CONFIG', 'SET', 'hz', '100')
+    env.cmd('CONFIG', 'SET', 'active-defrag-ignore-bytes', '1')
+    env.cmd('CONFIG', 'SET', 'active-defrag-threshold-lower', '0')
+    env.cmd('CONFIG', 'SET', 'active-defrag-cycle-min', '99')
+
+    try:
+        env.cmd('CONFIG', 'SET', 'activedefrag', 'yes')
+    except Exception:
+        # If active defrag is not supported by the current Redis, simply skip the test.
+        env.skip()
+
+def defragOnObj(env, obj):
+    enableDefrag(env)
+    env.expect('JSON.SET', 'test', '$', json.dumps(obj)).ok()
+    _, _, _, _, _, keysDefrag = env.cmd('JSON.DEBUG', 'DEFRAG_INFO')
+    startTime = time.time()
+    # Wait for at least 2 defrag full cycles
+    # We verify only the 'keysDefrag' value because the other values
+    # are not promised to be updated. It depends if Redis support
+    # the start/end defrag callbacks.
+    while keysDefrag < 2:
+        time.sleep(0.1)
+        _, _, _, _, _, keysDefrag = env.cmd('JSON.DEBUG', 'DEFRAG_INFO')
+        if time.time() - startTime > 30:
+            # We will wait for up to 30 seconds and then we consider it a failure
+            env.assertTrue(False, message='Failed waiting for defrag to run')
+            return
+    # make sure json is still valid.
+    res = json.loads(env.cmd('JSON.GET', 'test', '$'))[0]
+    env.assertEqual(res, obj)
+
+def testDefragNumber(env):
+    defragOnObj(env, 1)
+
+def testDefragBigNumber(env):
+    defragOnObj(env, 100000000000000000000)
+    
+def testDefragDouble(env):
+    defragOnObj(env, 1.111111111111)
+
+def testDefragNegativeNumber(env):
+    defragOnObj(env, -100000000000000000000)
+
+def testDefragNegativeDouble(env):
+    defragOnObj(env, -1.111111111111)
+
+def testDefragTrue(env):
+    defragOnObj(env, True)
+
+def testDefragFalse(env):
+    defragOnObj(env, True)
+
+def testDefragNone(env):
+    defragOnObj(env, None)
+
+def testDefragEmptyString(env):
+    defragOnObj(env, "")
+
+def testDefragString(env):
+    defragOnObj(env, "foo")
+
+def testDefragEmptyArray(env):
+    defragOnObj(env, [])
+
+def testDefragArray(env):
+    defragOnObj(env, [1, 2, 3])
+
+def testDefragEmptyObject(env):
+    defragOnObj(env, {})
+
+def testDefragObject(env):
+    defragOnObj(env, {"foo": "bar"})
+
+def testDefragComplex(env):
+    defragOnObj(env, {"foo": ["foo", 1, None, True, False, {}, {"foo": [], "bar": 1}]})
