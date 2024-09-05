16ec800e3dfd7cbd1c07cc262279f913a97d3c47
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-09-05 12:12:38 +0300

    .

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -20,4 +20,6 @@ $MODE apt -y install python3.8 python3.8-venv python3.8-dev python3-venv python3
 # Set python3 to point to python3.8
 $MODE update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
 
+pip install -q addict toml jinja2 ramp-packer
+
 source install_cmake.sh $MODE
