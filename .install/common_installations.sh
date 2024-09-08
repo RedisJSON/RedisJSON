83fee5d0b9241f90fc9025f9e1a3ed6012efe4f8
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-09-08 12:29:00 +0300

    wrong pip

--- .install/common_installations.sh
+++ .install/common_installations.sh
@@ -3,12 +3,12 @@ set -e
 OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
 
-pip install --upgrade pip
-pip install -q --upgrade setuptools
+pip3 install --upgrade pip
+pip3 install -q --upgrade setuptools
 echo "pip version: $(pip3 --version)"
 echo "pip path: $(which pip3)"
 
-pip install -q -r tests/pytest/requirements.txt
+pip3 install -q -r -v --no-build-isolation --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" tests/pytest/requirements.txt
 
 # List installed packages
-pip list
+pip3 list
