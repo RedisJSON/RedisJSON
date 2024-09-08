1112399b366823ee0a6ff82c726797662f34403e
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-09-08 11:53:38 +0300

    .

--- .install/common_installations.sh
+++ .install/common_installations.sh
@@ -3,12 +3,12 @@ set -e
 OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
 
-pip3 install --upgrade pip
-pip3 install -q --upgrade setuptools
+pip install --upgrade pip
+pip install -q --upgrade setuptools
 echo "pip version: $(pip3 --version)"
 echo "pip path: $(which pip3)"
 
-pip3 install -q -r tests/pytest/requirements.txt
+pip install -v --no-build-isolation --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" -q -r tests/pytest/requirements.txt
 
 # List installed packages
-pip3 list
+pip list
