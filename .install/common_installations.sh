86c6d406cebf273c54387694eb4fe8d4ea9591b7
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-09-08 12:43:11 +0300

    .

--- .install/common_installations.sh
+++ .install/common_installations.sh
@@ -8,7 +8,7 @@ pip3 install -q --upgrade setuptools
 echo "pip version: $(pip3 --version)"
 echo "pip path: $(which pip3)"
 
-pip3 install -q -v --no-build-isolation --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" -r ./tests/pytest/requirements.txt
+pip3 install -q -v --no-build-isolation --no-cache-dir -r ./tests/pytest/requirements.txt
 
 # List installed packages
 pip3 list
