09dbadd76f9da04fbb56a4efdb7e7d8608d9d790
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-09-08 16:03:34 +0300

    upgrade

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -4,6 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
+$MODE apt upgrade -yqq
 $MODE apt install -yqq git wget build-essential lcov openssl libssl-dev \
   python3 python3-pip python3-venv python3-dev unzip rsync libclang-dev clang curl
 source install_cmake.sh $MODE
