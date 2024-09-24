90a03cd63eb40808c0d6bc55c8828c9d07433429
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-09-24 16:41:13 +0300

    pass sudo along

--- .install/install_clang.sh
+++ .install/install_clang.sh
@@ -3,6 +3,7 @@
 export CWD=$(dirname `which "${0}"`)
 export CLANG_VERSION=18
 export DEBIAN_FRONTEND=noninteractive
+MODE=$1 # whether to install using sudo or not
 
 wget https://apt.llvm.org/llvm.sh -O llvm.sh
 
@@ -15,4 +16,4 @@ apt-get install python3-lldb-18 --yes --force-yes
 
 ./llvm.sh $CLANG_VERSION
 
-$CWD/update_clang_alternatives.sh $CLANG_VERSION 1
+$MODE $CWD/update_clang_alternatives.sh $CLANG_VERSION 1
