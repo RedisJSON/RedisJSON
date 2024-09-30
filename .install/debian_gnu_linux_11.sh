22afbf7d7423b769c3631253f6c4b05d325cbbc8
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-09-30 14:37:59 +0300

    remove extraneous cargo download?

--- .install/debian_gnu_linux_11.sh
+++ .install/debian_gnu_linux_11.sh
@@ -6,6 +6,6 @@ MODE=$1 # whether to install using sudo or not
 $MODE apt update -qq
 $MODE apt upgrade -yqq
 $MODE apt install -yqq git wget curl build-essential lcov openssl libssl-dev python3 python3-venv python3-pip \
-  rsync unzip cargo libclang-dev clang
+  rsync unzip libclang-dev clang
 
 source install_cmake.sh $MODE
