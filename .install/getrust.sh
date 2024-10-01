5b72308ab420d2c1220b8a544fe6d40e5a8591d6
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-10-01 11:44:15 +0300

    $HOME -> ~

--- .install/getrust.sh
+++ .install/getrust.sh
@@ -6,8 +6,8 @@ MODE=$1 # whether to install using sudo or not
 $MODE curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Source the cargo environment script to update the PATH
-echo "source $HOME/.cargo/env" >> $HOME/.bashrc
-source $HOME/.cargo/env
+echo "source ~/.cargo/env" >> ~/.bashrc
+source ~/.cargo/env
 
 # Update rustup and install nightly toolchain
 $MODE rustup update
