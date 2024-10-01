f98adfaf102c19197f28a64b1c2d3d794ef0bbf6
efeldblum <109016390+ephraimfeldblum@users.noreply.github.com>
2024-10-01 15:17:55 +0300

    GHA step: Sanitizer (#1274)
    * initial commit
    
    * tryst
    
    * actions/checkout
    
    * shell: bash
    
    * shell: bash
    
    * id
    
    * shell: bash
    
    * sudo
    
    * no install git
    
    * container
    
    * container
    
    * delay common_installations
    
    * getrust
    
    * remove extraneous cargo download?
    
    * uninstall and reinstall
    
    * profile_d
    
    * bullseye -> jammy
    
    * focal
    
    * rocky9?
    
    * move installing cargo to immediately before making redisjson
    
    * jammy
    
    * dir
    
    * revert
    
    * no default dir
    
    * no default dir
    
    * unneeded if
    
    * rust
    
    * profile.d
    
    * working dir
    
    * env
    
    * echo to bash_profile
    
    * source
    
    * echo
    
    * pwd
    
    * bashrc
    
    * echo groups
    
    * no sudo
    
    * $HOME -> ~
    
    * revert
    
    * hardcoding it in
    
    * fix alpine arm
    
    * fix alpine arm
    
    * fix alpine arm
    
    * fix alpine arm
    
    * review
    
    * review
    
    (cherry picked from commit c94ee010735dda3997cba7ede72aefae1d3f8a6c)

--- .install/getrust.sh
+++ .install/getrust.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+MODE=$1 # whether to install using sudo or not
+
 # Download and install rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+$MODE curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Source the cargo environment script to update the PATH
-source "$HOME/.cargo/env"
+echo "source $HOME/.cargo/env" >> $HOME/.bashrc
+source $HOME/.cargo/env
 
 # Update rustup and install nightly toolchain
-rustup update
-rustup update nightly
+$MODE rustup update
+$MODE rustup update nightly
 
 # for RedisJSON build with addess santizer
-rustup component add rust-src --toolchain nightly
+$MODE rustup component add --toolchain nightly rust-src
 
 # Verify cargo installation
 cargo --version
+
+rustup show
