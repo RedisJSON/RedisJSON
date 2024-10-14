2aeb410b4888202c5b6a02e17fbc78749774d393
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-10-14 11:07:36 +0300

    i hate ai tools

--- .install/common_installations.sh
+++ .install/common_installations.sh
@@ -3,6 +3,19 @@ set -e
 OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
 
+activate_venv() {
+	echo "copy ativation script to shell config"
+	if [[ $OS_TYPE == Darwin ]]; then
+		echo "source venv/bin/activate" >> ~/.bashrc
+		echo "source venv/bin/activate" >> ~/.zshrc
+	else
+		echo "source $PWD/venv/bin/activate" >> ~/.bash_profile
+	fi
+}
+python3 -m venv venv
+activate_venv
+source venv/bin/activate
+
 pip3 install --upgrade pip
 pip3 install -q --upgrade setuptools
 echo "pip version: $(pip3 --version)"
