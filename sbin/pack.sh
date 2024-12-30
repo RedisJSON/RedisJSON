a1225f9ef8ac2482335781843ae37ceb41acbaa7
Eran Hadad <eran.hadad@redis.com>
2024-12-30 10:20:12 +0200

    MOD-8436 update upload artifacts script to use Ventura and Sonoma instead catalina and monterey (#1314)
    (cherry picked from commit c15c0f00f868bb7cea01170e5685afc5753eb61c)

--- sbin/pack.sh
+++ sbin/pack.sh
@@ -70,15 +70,6 @@ OSNICK=$($READIES/bin/platform --osnick)
 [[ $OSNICK == ol8 ]]     && OSNICK=rhel8
 [[ $OSNICK == rocky8 ]]  && OSNICK=rhel8
 
-if [[ $OS == macos ]]; then
-	# as we don't build on macOS for every platform, we converge to a least common denominator
-	if [[ $ARCH == x86_64 ]]; then
-		OSNICK=catalina  # to be aligned with the rest of the modules in redis stack
-	else
-		[[ $OSNICK == ventura ]] && OSNICK=monterey
-	fi
-fi
-
 PLATFORM="$OS-$OSNICK-$ARCH"
 
 #----------------------------------------------------------------------------------------------
