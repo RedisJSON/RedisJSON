c9ff3f7ba2468ab56ad3911bd9758aaf97d5b24d
jonathan keinan <jonathan.keinan@redis.com>
2025-02-05 18:29:53 +0200

    * use all option in llvm.sh

--- .install/install_clang.sh
+++ .install/install_clang.sh
@@ -9,12 +9,10 @@ wget https://apt.llvm.org/llvm.sh -O llvm.sh
 
 chmod u+x llvm.sh
 
-# expected to fail:
-$MODE ./llvm.sh $CLANG_VERSION
+$MODE ./llvm.sh $CLANG_VERSION all
 
 $MODE apt-get install python3-lldb-18 --yes --force-yes
 
-$MODE ./llvm.sh $CLANG_VERSION
 
 $MODE $CWD/update_clang_alternatives.sh $CLANG_VERSION 1
 
