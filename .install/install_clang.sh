760aa2ba5d52bfdbaef5b17003439470d0665624
Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
2025-02-06 13:39:26 +0200

    MOD-8480: update redismodule-rs to 2.1.2 and fix codecov (#1326)
    * MOD-8480: update redismodule-rs to 2.1.2 on master
    
    * * try building with newer readies
    
    * turn on shell debug mode
    
    * update readies commit
    
    * * print coverage tools version
    
    * * fix clang-format not being a slave in update_clang_alternatives.sh
    
    * * print clang++ version
    
    * * cleanup a bit
    
    * * use all option in llvm.sh
    
    * * more generic update alternatives script
    
    * * test
    
    * * small fix
    
    * * try removing before installing
    
    * * fix deprecated default_features by moving it to be default-features
    
    * * print clang version
    
    * * update rust
    
    * * update coverage flag
    
    * * use rustc llvm version to avoid versioning errors in code coverage
    
    ---------
    
    Co-authored-by: jonathan keinan <jonathan.keinan@redis.com>
    (cherry picked from commit 1da355d309bdd36729d016ae833b8011e0791fb4)

--- .install/install_clang.sh
+++ .install/install_clang.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
+rust_llvm_major_version=$(rustc --version --verbose | grep "LLVM version" | awk '{print $3}' | cut -d. -f1)
+
 export CWD=$(dirname `which "${0}"`)
-export CLANG_VERSION=18
+export CLANG_VERSION=${rust_llvm_major_version}
 export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
@@ -9,11 +11,16 @@ wget https://apt.llvm.org/llvm.sh -O llvm.sh
 
 chmod u+x llvm.sh
 
-# expected to fail:
-$MODE ./llvm.sh $CLANG_VERSION
+$MODE ./llvm.sh $CLANG_VERSION all
 
-$MODE apt-get install python3-lldb-18 --yes --force-yes
-
-$MODE ./llvm.sh $CLANG_VERSION
+$MODE apt-get install python3-lldb-${rust_llvm_major_version} --yes
 
 $MODE $CWD/update_clang_alternatives.sh $CLANG_VERSION 1
+
+ls /bin/
+
+$MODE clang --version
+$MODE clang++ --version
+$MODE llvm-cov --version
+$MODE llvm-profdata --version
+
