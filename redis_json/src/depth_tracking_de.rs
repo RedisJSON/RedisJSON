52ac0708b811b40ba21200b96413cc1ef1544c79
avivdavid23 <avivdavid2312@gmail.com>
2026-01-27 11:13:13 +0200

    performance

--- redis_json/src/depth_tracking_de.rs
+++ redis_json/src/depth_tracking_de.rs
@@ -1,51 +1,20 @@
 use serde::de::{self, DeserializeSeed, MapAccess, SeqAccess, Visitor};
 use serde_json::Deserializer as JsonDeserializer;
-use std::cell::RefCell;
-use std::rc::Rc;
-
-#[derive(Clone)]
-struct DepthTracker {
-    current: Rc<RefCell<usize>>,
-    max: Rc<RefCell<usize>>,
-}
-
-impl DepthTracker {
-    fn new() -> Self {
-        Self {
-            current: Rc::new(RefCell::new(0)),
-            max: Rc::new(RefCell::new(0)),
-        }
-    }
-
-    fn enter(&self) {
-        let mut current = self.current.borrow_mut();
-        *current += 1;
-        let mut max = self.max.borrow_mut();
-        if *current > *max {
-            *max = *current;
-        }
-    }
-
-    fn exit(&self) {
-        let mut current = self.current.borrow_mut();
-        *current = current.saturating_sub(1);
-    }
-
-    fn max_depth(&self) -> usize {
-        *self.max.borrow()
-    }
-}
+use std::cell::Cell;
+use std::ptr::NonNull;
 
 pub struct DepthTrackingDeserializer<'de> {
     de: JsonDeserializer<serde_json::de::StrRead<'de>>,
-    tracker: DepthTracker,
+    current_depth: Cell<usize>,
+    max_depth: Cell<usize>,
 }
 
 impl<'de> DepthTrackingDeserializer<'de> {
     pub fn from_str(s: &'de str) -> Self {
         Self {
             de: JsonDeserializer::from_str(s),
-            tracker: DepthTracker::new(),
+            current_depth: Cell::new(0),
+            max_depth: Cell::new(0),
         }
     }
 
@@ -54,7 +23,41 @@ impl<'de> DepthTrackingDeserializer<'de> {
     }
 
     pub fn max_depth(&self) -> usize {
-        self.tracker.max_depth()
+        self.max_depth.get()
+    }
+
+    fn tracker(&self) -> DepthTracker {
+        DepthTracker {
+            current: NonNull::from(&self.current_depth),
+            max: NonNull::from(&self.max_depth),
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+struct DepthTracker {
+    current: NonNull<Cell<usize>>,
+    max: NonNull<Cell<usize>>,
+}
+
+impl DepthTracker {
+    #[inline]
+    fn enter(&self) {
+        // SAFETY: The DepthTrackingDeserializer owns these cells and outlives all uses
+        let current = unsafe { self.current.as_ref() };
+        let max = unsafe { self.max.as_ref() };
+        let new = current.get() + 1;
+        current.set(new);
+        if new > max.get() {
+            max.set(new);
+        }
+    }
+
+    #[inline]
+    fn exit(&self) {
+        // SAFETY: Same as above
+        let current = unsafe { self.current.as_ref() };
+        current.set(current.get() - 1);
     }
 }
 
@@ -70,6 +73,7 @@ impl<'de, V: Visitor<'de>> Visitor<'de> for TrackingVisitor<V> {
         self.inner.expecting(formatter)
     }
 
+    #[inline]
     fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
     where
         A: MapAccess<'de>,
@@ -77,12 +81,13 @@ impl<'de, V: Visitor<'de>> Visitor<'de> for TrackingVisitor<V> {
         self.tracker.enter();
         let result = self.inner.visit_map(TrackingMapAccess {
             inner: map,
-            tracker: self.tracker.clone(),
+            tracker: self.tracker,
         });
         self.tracker.exit();
         result
     }
 
+    #[inline]
     fn visit_seq<A>(self, seq: A) -> Result<Self::Value, A::Error>
     where
         A: SeqAccess<'de>,
@@ -90,36 +95,53 @@ impl<'de, V: Visitor<'de>> Visitor<'de> for TrackingVisitor<V> {
         self.tracker.enter();
         let result = self.inner.visit_seq(TrackingSeqAccess {
             inner: seq,
-            tracker: self.tracker.clone(),
+            tracker: self.tracker,
         });
         self.tracker.exit();
         result
     }
 
+    #[inline]
     fn visit_bool<E: de::Error>(self, v: bool) -> Result<Self::Value, E> {
         self.inner.visit_bool(v)
     }
+
+    #[inline]
     fn visit_i64<E: de::Error>(self, v: i64) -> Result<Self::Value, E> {
         self.inner.visit_i64(v)
     }
+
+    #[inline]
     fn visit_u64<E: de::Error>(self, v: u64) -> Result<Self::Value, E> {
         self.inner.visit_u64(v)
     }
+
+    #[inline]
     fn visit_f64<E: de::Error>(self, v: f64) -> Result<Self::Value, E> {
         self.inner.visit_f64(v)
     }
+
+    #[inline]
     fn visit_str<E: de::Error>(self, v: &str) -> Result<Self::Value, E> {
         self.inner.visit_str(v)
     }
+
+    #[inline]
     fn visit_string<E: de::Error>(self, v: String) -> Result<Self::Value, E> {
         self.inner.visit_string(v)
     }
+
+    #[inline]
     fn visit_unit<E: de::Error>(self) -> Result<Self::Value, E> {
         self.inner.visit_unit()
     }
+
+    #[inline]
     fn visit_none<E: de::Error>(self) -> Result<Self::Value, E> {
         self.inner.visit_none()
     }
+
+    #[inline]
     fn visit_some<D: de::Deserializer<'de>>(
         self,
         deserializer: D,
@@ -136,6 +158,7 @@ struct TrackingSeed<S> {
 impl<'de, S: DeserializeSeed<'de>> DeserializeSeed<'de> for TrackingSeed<S> {
     type Value = S::Value;
 
+    #[inline]
     fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
     where
         D: de::Deserializer<'de>,
@@ -155,6 +178,7 @@ struct TrackingDeserializerGeneric<D> {
 impl<'de, D: de::Deserializer<'de>> de::Deserializer<'de> for TrackingDeserializerGeneric<D> {
     type Error = D::Error;
 
+    #[inline]
     fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
@@ -165,6 +189,7 @@ impl<'de, D: de::Deserializer<'de>> de::Deserializer<'de> for TrackingDeserializ
         })
     }
 
+    #[inline]
     fn deserialize_map<V>(self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
@@ -175,6 +200,7 @@ impl<'de, D: de::Deserializer<'de>> de::Deserializer<'de> for TrackingDeserializ
         })
     }
 
+    #[inline]
     fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
@@ -185,6 +211,7 @@ impl<'de, D: de::Deserializer<'de>> de::Deserializer<'de> for TrackingDeserializ
         })
     }
 
+    #[inline]
     fn deserialize_struct<V>(
         self,
         name: &'static str,
@@ -219,6 +246,7 @@ struct TrackingMapAccess<A> {
 impl<'de, A: MapAccess<'de>> MapAccess<'de> for TrackingMapAccess<A> {
     type Error = A::Error;
 
+    #[inline]
     fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, Self::Error>
     where
         K: DeserializeSeed<'de>,
@@ -226,13 +254,14 @@ impl<'de, A: MapAccess<'de>> MapAccess<'de> for TrackingMapAccess<A> {
         self.inner.next_key_seed(seed)
     }
 
+    #[inline]
     fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error>
     where
         V: DeserializeSeed<'de>,
     {
         self.inner.next_value_seed(TrackingSeed {
             inner: seed,
-            tracker: self.tracker.clone(),
+            tracker: self.tracker,
         })
     }
 }
@@ -245,13 +274,14 @@ struct TrackingSeqAccess<A> {
 impl<'de, A: SeqAccess<'de>> SeqAccess<'de> for TrackingSeqAccess<A> {
     type Error = A::Error;
 
+    #[inline]
     fn next_element_seed<T>(&mut self, seed: T) -> Result<Option<T::Value>, Self::Error>
     where
         T: DeserializeSeed<'de>,
     {
         self.inner.next_element_seed(TrackingSeed {
             inner: seed,
-            tracker: self.tracker.clone(),
+            tracker: self.tracker,
         })
     }
 }
@@ -259,13 +289,15 @@ impl<'de, A: SeqAccess<'de>> SeqAccess<'de> for TrackingSeqAccess<A> {
 impl<'de> de::Deserializer<'de> for &mut DepthTrackingDeserializer<'de> {
     type Error = serde_json::Error;
 
+    #[inline]
     fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
+        let tracker = self.tracker();
         self.de.deserialize_any(TrackingVisitor {
             inner: visitor,
-            tracker: self.tracker.clone(),
+            tracker,
         })
     }
 
