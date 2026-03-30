e2ef102107c81b32993860bd36d7c044c659d66b
avivdavid23 <avivdavid2312@gmail.com>
2026-03-30 10:28:05 +0300

    add namespace for enum

--- redis_json/src/include/rejson_api.h
+++ redis_json/src/include/rejson_api.h
@@ -28,31 +28,31 @@ typedef enum JSONType {
 
 typedef enum JSONArrayType {
   /// Array contains heterogeneous IValue objects
-  Heterogeneous = 0,
+  JSONArrayType_Heterogeneous = 0,
   /// Array contains i8 values
-  I8 = 1,
+  JSONArrayType_I8 = 1,
   /// Array contains u8 values
-  U8 = 2,
+  JSONArrayType_U8 = 2,
   /// Array contains i16 values
-  I16 = 3,
+  JSONArrayType_I16 = 3,
   /// Array contains u16 values
-  U16 = 4,
+  JSONArrayType_U16 = 4,
   /// Array contains f16 values
-  F16 = 5,
+  JSONArrayType_F16 = 5,
   /// Array contains bf16 values
-  BF16 = 6,
+  JSONArrayType_BF16 = 6,
   /// Array contains i32 values
-  I32 = 7,
+  JSONArrayType_I32 = 7,
   /// Array contains u32 values
-  U32 = 8,
+  JSONArrayType_U32 = 8,
   /// Array contains f32 values
-  F32 = 9,
+  JSONArrayType_F32 = 9,
   /// Array contains i64 values
-  I64 = 10,
+  JSONArrayType_I64 = 10,
   /// Array contains u64 values
-  U64 = 11,
+  JSONArrayType_U64 = 11,
   /// Array contains f64 values
-  F64 = 12,
+  JSONArrayType_F64 = 12,
 } JSONArrayType;
 
 typedef const void* RedisJSON;
@@ -167,7 +167,7 @@ typedef struct RedisJSONAPI {
   ////////////////
   // Return a pointer to the array and the length of the array
   // If `json` is not an array, return NULL and set len to 0
-  // If type is JSONArrayType::Heterogeneous, do not use the returned buffer,
+  // If type is JSONArrayType::JSONArrayType_Heterogeneous, do not use the returned buffer,
   // use the previous array API to get the values(e.g. getAt, etc.)
   const void* (*getArray)(RedisJSON json, size_t *len, JSONArrayType *type);
 
