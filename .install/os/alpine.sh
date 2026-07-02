ef8046cd8950798a8eefcde44c9831a975a776dd
TalBarYakar <talbaryakar@gmail.com>
2026-07-02 16:21:29 +0300

    fix for alpine

--- .install/os/alpine.sh
+++ .install/os/alpine.sh
@@ -5,3 +5,12 @@
 
 alpine_default_install
 apk_install py3-cryptography py3-numpy py3-psutil openblas-dev xsimd
+
+# bindgen uses dlopen to load libclang.so. Alpine ships only versioned symlinks.
+if [ ! -e /usr/lib/libclang.so ]; then
+    if [ -e /usr/lib/libclang.so.21.1 ]; then
+        ln -sf libclang.so.21.1 /usr/lib/libclang.so
+    elif [ -e /usr/lib/libclang.so.18.1 ]; then
+        ln -sf libclang.so.18.1 /usr/lib/libclang.so
+    fi
+fi
