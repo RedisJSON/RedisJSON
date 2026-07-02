5150ffb6b1e5d5c7816f444ccb66fce08e1532bd
TalBarYakar <talbaryakar@gmail.com>
2026-07-02 16:21:29 +0300

    fix mariner

--- .install/os/mariner2.sh
+++ .install/os/mariner2.sh
@@ -8,4 +8,6 @@ tdnf_default_install
 # Install aws-cli for uploading artifacts to s3. Subshell at /tmp keeps the
 # zip and extracted ./aws/ directory out of the caller's CWD (the script is
 # sourced from install_script.sh with CWD at .install/).
-(cd /tmp && curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip && unzip -qo awscliv2.zip && ./aws/install)
+ARCH=$(uname -m)
+[[ "$ARCH" == "aarch64" ]] && ARCH="aarch64" || ARCH="x86_64"
+(cd /tmp && curl "https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}.zip" -o awscliv2.zip && unzip -qo awscliv2.zip && ./aws/install --update)
