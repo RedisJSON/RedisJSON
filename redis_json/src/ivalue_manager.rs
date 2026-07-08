dcc85f8e571d4cd10b49b56419d5be178a9bf286
TalBarYakar <talbaryakar@gmail.com>
2026-07-08 14:48:53 +0300

    fix clippy

--- redis_json/src/ivalue_manager.rs
+++ redis_json/src/ivalue_manager.rs
@@ -776,6 +776,7 @@ impl<'a> Manager for RedisIValueJsonKeyManager<'a> {
         Ok(IValueKeyHolderRead { key })
     }
 
+    #[allow(clippy::not_unsafe_ptr_arg_deref)]
     fn open_key_from_handle(&self, key: *mut RedisModuleKey) -> *const IValue {
         let value = unsafe { rawmod::RedisModule_ModuleTypeGetValue.unwrap()(key) }
             .cast::<RedisJSON<IValue>>();
